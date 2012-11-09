8fccc08fd3f24be71b2ab3421f8fb37d521365b5
flux <adityavkd@gmail.com>
2012-11-09 11:25:47 +0530

    Added UIViewController subclass for rotation

---
+++ Classes/OLViewController.h
@@ -0,0 +1,4 @@
+#import <UIKit/UIKit.h>
+
+@interface OLViewController : UIViewController
+@end
---
+++ Classes/OLViewController.m
@@ -0,0 +1,10 @@
+#import "OLViewController.h"
+
+@implementation OLViewController
+
+- (BOOL)shoudAutoRotate
+{
+	return YES;
+}
+
+@end
